1ec89e6c6a38c94d3adeb01d9f1ccdd7528c3b6b
ottosmaniotto <you@example.com>
2018-03-08 16:48:19 -0300

    react hot loader

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { hot } from 'react-hot-loader';
 import {Route, Switch, BrowserRouter} from 'react-router-dom';
 
 import { Provider } from "react-redux";
@@ -27,4 +28,4 @@ class App extends Component {
     }
 }
 
-export default App;
+export default hot(module)(App)
